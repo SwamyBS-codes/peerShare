f0fd532d0eb822bdb3e1efec6bb8baf57897c3af
18bharathkumar <bharathms8055@gmail.com>
2026-08-31 00:51:15 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const BACKEND_URL = 'https://peershare.duckdns.org/';
+const BACKEND_URL = 'https://peershare.duckdns.org';
 
 export default defineConfig({
   plugins: [react()],
