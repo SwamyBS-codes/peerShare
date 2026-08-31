f5b8cdf2b85994db864f69b67aeca862409771c2
Swamy B S <156047448+SwamyBS-codes@users.noreply.github.com>
2026-08-31 15:39:30 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const BACKEND_URL = 'https://peershare.duckdns.org';
+const BACKEND_URL = 'https://peershare-signalling.duckdns.org';
 
 // const BACKEND_URL = 'http://localhost:3001';
 
